7c2f5744141cac0c403230714494a68ed5427c8a
SVTsygankov <s.v.tsygankov@mail.ru>
2025-01-13 16:08:59 +0500

    Lesson-43 integration test

--- src/main/java/com/svtsygankov/spring/config/ApplicationConfiguration.java
+++ src/main/java/com/svtsygankov/spring/config/ApplicationConfiguration.java
@@ -1,7 +1,6 @@
 package com.svtsygankov.spring.config;
 
 import com.svtsygankov.spring.database.pool.ConnectionPool;
-import com.svtsygankov.spring.database.repository.CrudRepository;
 import com.svtsygankov.spring.database.repository.UserRepository;
 import com.svtsygankov.web.WebConfiguration;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -11,7 +10,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Scope;
 
 
