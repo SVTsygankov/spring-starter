16f6d5d3e99e118af0e324a1348cf2f2b67759e0
SVTsygankov <s.v.tsygankov@mail.ru>
2025-01-21 15:14:03 +0500

    Lesson-59 Special parameters Sort & Pageable

--- src/test/java/com/svtsygankov/spring/integration/database/repository/UserRepositoryTest.java
+++ src/test/java/com/svtsygankov/spring/integration/database/repository/UserRepositoryTest.java
@@ -5,19 +5,44 @@ import com.svtsygankov.spring.database.entity.User;
 import com.svtsygankov.spring.database.repository.UserRepository;
 import com.svtsygankov.spring.integration.annotation.IT;
 import lombok.RequiredArgsConstructor;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @IT
 @RequiredArgsConstructor
 class UserRepositoryTest {
 
     private final UserRepository userRepository;
+
+    @Test
+    void checkPageable() {
+        var pageable = PageRequest.of(1, 2, Sort.by("id"));
+        var result = userRepository.findAllBy(pageable);
+        assertThat(result).hasSize(2);
+
+    }
+
+    @Test
+    void checkSort() {
+        var sortBy = Sort.sort(User.class);
+        var sort = sortBy.by(User::getFirstname).and(sortBy.by(User::getLastname));
+        //        var sortById = Sort.by("id");
+
+//        var sortById = sortBy.by("firstname").and(sortBy.by("lastname"));
+
+
+        var users = userRepository.findTop3ByBirthDateBefore(LocalDate.now(), sort);
+        assertThat(users).hasSize(3);
+    }
 
     @Test
     void checkUpdate() {
@@ -36,6 +61,14 @@ class UserRepositoryTest {
     @Test
     void checkQueries() {
         var users = userRepository.findAllBy("a", "ov");
-        Assertions.assertThat(users).hasSize(3);
+        assertThat(users).hasSize(3);
+    }
+
+    @Test
+    void checkFirstTop () {
+
+        var topUser= userRepository.findTopByOrderByIdDesc();
+        assertTrue(topUser.isPresent());
+        topUser.ifPresent(user -> assertEquals(5L, user.getId()));
     }
 }
