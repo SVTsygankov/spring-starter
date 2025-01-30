6bc76971bac7fdcd96df3a0c9df99886b55c78bb
SVTsygankov <s.v.tsygankov@mail.ru>
2025-01-30 14:54:02 +0500

    Lesson-78 Parameters Headers Cookies

--- src/main/java/com/svtsygankov/spring/http/controller/GreetingController.java
+++ src/main/java/com/svtsygankov/spring/http/controller/GreetingController.java
@@ -1,16 +1,30 @@
 package com.svtsygankov.spring.http.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/api/v1")
 public class GreetingController {
 
-    @GetMapping("/hello")
-    public ModelAndView hello(ModelAndView modelAndView) {
+    @GetMapping("/hello/{id}")
+    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request,
+                              @RequestParam Integer age,
+                              @RequestHeader String accept,
+                              @CookieValue("JSESSIONID") String jsessionId,
+                              @PathVariable("id") Integer id) {
+        var ageParamValue = request.getParameter("age");
+        var acceptHeder = request.getHeader("accept");
+        var cookies = request.getCookies();
         modelAndView.setViewName("greeting/hello");
         return modelAndView;
     }
