c855626e1acf39f3414935669bb906b8b5dea21d
SVTsygankov <s.v.tsygankov@mail.ru>
2025-03-04 14:39:13 +0500

    Lesson-106 authorization architecture

--- src/main/java/com/svtsygankov/spring/config/SecurityConfiguration.java
+++ src/main/java/com/svtsygankov/spring/config/SecurityConfiguration.java
@@ -2,11 +2,12 @@ package com.svtsygankov.spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static com.svtsygankov.spring.database.entity.Role.ADMIN;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -15,17 +16,21 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .authorizeRequests().anyRequest().authenticated()
-                .and()
+                .authorizeHttpRequests(urlConfig -> urlConfig
+                        .antMatchers("/login", "/users/registration", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
+                        .antMatchers("/users/{\\d+}/delete").hasAuthority(ADMIN.getAuthority())
+                        .antMatchers("/admin/**").hasAuthority(ADMIN.getAuthority())
+                        .anyRequest().authenticated()
+                )
+
 //                .httpBasic(Customizer.withDefaults());
-                .formLogin(login -> login
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/users")
-                        .permitAll())
                 .logout(logout -> logout
                         .logoutUrl("/logout")                    // Это
                         .logoutSuccessUrl("/login")         //     дефолтное
-                        .deleteCookies("JSESSIONID")); //              поведение
+                        .deleteCookies("JSESSIONID")) //              поведение
+                .formLogin(login -> login
+                .loginPage("/login")
+                .defaultSuccessUrl("/users"));
     }
 
     @Bean
